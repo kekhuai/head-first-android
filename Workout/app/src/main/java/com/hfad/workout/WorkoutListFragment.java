4dad717533f92be1bb741dd93a1ae87725e91d07
Natthaphon Phanit <natthaphon@geniustree.co.th>
2017-08-28 11:19:16 +0700

    chapter7 completed

---
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -0,0 +1,54 @@
+package com.hfad.workout;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.stream.Stream;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+
+    interface WorkoutListListener {
+        void itemClicked(long id);
+    }
+
+    private WorkoutListListener listener;
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String[] names = new String[Workout.WORKOUTS.length];
+        for (int i = 0; i < names.length; ++i) {
+            names[i] = Workout.WORKOUTS[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.listener = (WorkoutListListener) activity;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (listener != null) {
+            listener.itemClicked(id);
+        }
+    }
+}
