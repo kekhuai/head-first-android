4cac0e443c89773e665cc3b99b9b4c9b726d7fdd
Natthaphon Phanit <70329@nttdth.com>
2017-09-05 11:04:42 +0700

    chapter10 completed

--- Bitsandpizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ Bitsandpizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,21 +1,125 @@
 package com.hfad.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.ActionProvider;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
+
+import layout.PastaFragment;
+import layout.PizzaFragment;
+import layout.StoresFragment;
 
 public class MainActivity extends Activity {
 
+    private String[] titles;
+
+    private ListView drawerList;
+
     private ShareActionProvider shareActionProvider;
+
+    DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    private int currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, getResources().getStringArray(R.array.titles)));
+        drawerList.setOnItemClickListener((parent, view, position, id) -> selectItem(position));
+        if (savedInstanceState == null) {
+            selectItem(0);
+        } else {
+            currentPosition = savedInstanceState.getInt("position");
+            selectItem(currentPosition);
+        }
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.addDrawerListener(drawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        getFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                FragmentManager fragMan = getFragmentManager();
+                Fragment fragment = fragMan.findFragmentByTag("visible_fragment");
+                if (fragment instanceof TopFragment) {
+                    currentPosition = 0;
+                } else if (fragment instanceof PizzaFragment) {
+                    currentPosition = 1;
+                } else if (fragment instanceof PastaFragment) {
+                    currentPosition = 2;
+                } else if (fragment instanceof StoresFragment) {
+                    currentPosition = 3;
+                }
+                setActionTitleBar(currentPosition);
+                drawerList.setItemChecked(currentPosition, true);
+            }
+        });
+    }
+
+    private void selectItem(int position) {
+        currentPosition = position;
+        Fragment fragment;
+        switch (position) {
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment, "visible_fragment");
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+        setActionTitleBar(position);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private void setActionTitleBar(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
     }
 
     @Override
@@ -29,6 +133,9 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()) {
             case R.id.action_create_order:
                 Intent intent = new Intent(this, OrderActivity.class);
@@ -46,5 +153,30 @@ public class MainActivity extends Activity {
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("position", currentPosition);
     }
 }
